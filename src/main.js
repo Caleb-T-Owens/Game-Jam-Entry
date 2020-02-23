37ea6350380343577822adddb6bf86c047ed7479
PinkFlufflyLlama <Cocacola5010mc@gmail.com>
2020-02-23 12:41:22 +0000

    Added to test

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import { Coord } from "./coord.js";
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let tree = document.createElement("img");
-tree.height = 256;
-tree.width = 256;
+tree.height = 500;
+tree.width = 200;
 tree.src = "./tree.png";
 
 let post = () => {
@@ -13,8 +13,24 @@ let post = () => {
 };
 
 let entityList = [
-   new Entity(new Coord(1,0), tree),
+   new Entity(new Coord(5,0), tree, "tree"),
+   new Entity(new Coord(5,5), tree, "tree"),
+   new Entity(new Coord(5,3), tree, "tree"),
+   new Entity(new Coord(2,8), tree, "tree"),
+   new Entity(new Coord(1,2), tree, "tree"),
+   new Entity(new Coord(0,5), tree, "tree"),
 ];
 
 let renderer = new Renderer(post, ctx, entityList);
 renderer.loop();
+
+document.addEventListener("keyup", (event) => {
+   if (event.key == "w") {
+      renderer.cameraPosition.x += 0.2;
+      renderer.loop();
+   }
+   if (event.key == "s") {
+      renderer.cameraAngle += 0.26;
+      renderer.loop();
+   }
+});
