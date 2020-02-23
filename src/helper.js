3735dbedac4b62bee389fc25e72841f6cd64281e
PinkFlufflyLlama <Cocacola5010mc@gmail.com>
2020-02-23 22:24:51 +0000

    Added javadoc

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,12 @@
 export class Helper {
+   /**
+    * @function
+    * @param {Number} value 
+    * @param {Number} istart 
+    * @param {Number} iend 
+    * @param {Number} ostart 
+    * @param {Number} oend 
+    */
    static map (value, istart, iend, ostart, oend) {
    return ostart + (oend - ostart) * ((value - istart) / (iend - istart));
    } 
