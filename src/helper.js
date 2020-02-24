644d941294502ee5ef7abb150b1291bfcd137e6b
PinkFlufflyLlama <Cocacola5010mc@gmail.com>
2020-02-24 18:13:49 +0000

    Added doc

--- src/helper.js
+++ src/helper.js
@@ -1,11 +1,11 @@
 export class Helper {
    /**
-    * @function
-    * @param {Number} value 
-    * @param {Number} istart 
-    * @param {Number} iend 
-    * @param {Number} ostart 
-    * @param {Number} oend 
+    * @function map maps a value to a range
+    * @param {Number} value the input value
+    * @param {Number} istart the start of the input range
+    * @param {Number} iend the end of the input range
+    * @param {Number} ostart the start of the output range
+    * @param {Number} oend the end of the output range
     */
    static map (value, istart, iend, ostart, oend) {
    return ostart + (oend - ostart) * ((value - istart) / (iend - istart));
